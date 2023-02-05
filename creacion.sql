eb0e71f4cd1b9808eb6a6e23a3e0b3c5ffe4941e
Angel Sainero Villar <angelsainero@gmail.com>
2023-02-05 19:24:34 +0100

    consulta

--- creacion.sql
+++ creacion.sql
@@ -585,24 +585,5 @@ insert into practicasql.revisiones_coche_hist
 values('015', '60000', '2020-11-11','132541','EUR','63,56');
 
 
---consulta 
 
---Nombre modelo, marca y grupo de coches (los nombre de todos)
--- Fecha de compra
--- Matricula
--- Nombre del color del coche
--- Total kilómetros
--- Nombre empresa que está asegurado el coche
--- Número de póliza
-
-select * from practicasql.revisiones
-
-select co.idcoche,gr.nombre, ma.nombre, mo.nombre, co.fechaCompra, co.matricula,  col.nombre, co.kmTotales, ase.nombre, aseh.numPoliza  
-from practicasql.coche co inner join practicasql.modelo mo on co.idmodelo=mo.idmodelo
-join practicasql.marca ma on mo.idMarca=ma.idMarca
-join practicasql.grupo gr on ma.idGrupo=gr.idGrupo
-join practicasql.colores col on co.idColor=col.idColor
-join practicasql.aseguradora_coche_hist aseh on co.idcoche = aseh.idcoche 
-join practicasql.aseguradoras ase on aseh.idaseguradora = ase.idaseguradora 
-;
 
